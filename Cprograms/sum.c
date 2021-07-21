596874263b6c3950cbf5a5ef012ef7ab5526644c
Deepak Gupta <spycaptain3@gmail.com>
2021-07-21 19:26:47 +0530

    basic sum using c

---
+++ Cprograms/sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the value of a and b:\n");
+    scanf("%d %d",&a,&b);
+    printf("Sum of %d and %d is %d",a,b,a+b);
+    return 0;
+}
