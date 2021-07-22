d0134af5f7d5bbaab359c97758ee0d7d396ff796
Deepak Gupta <spycaptain3@gmail.com>
2021-07-22 22:38:06 +0530

    freq of Char in a string with issue of counting +1

---
+++ Cprograms/Sheet_Q45.c
@@ -0,0 +1,24 @@
+//C program to find the frequency of Character in a string.
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[50];
+    char ch;
+    int i,j,n,count;
+    printf("Enter a string :\n");
+    fgets(str,50,stdin);
+    printf("Entered string is: %s\n",str);
+    printf("Enter the character to find frequency to it: \n");
+    scanf("%c",&ch);
+    for(i=0;str[i]!='\0';++i)
+    {
+        if(ch==str[i])
+        {
+            ++count;
+        }
+    }
+    printf("Frequency of %c is %d\n",ch,count);
+    return 0;
+}
