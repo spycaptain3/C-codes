c6e1e0dab8f9c68e4f03c1407659145b5b90b756
Deepak Gupta <spycaptain3@gmail.com>
2021-07-22 21:43:10 +0530

    Update class16julQ9.c

--- Cprograms/class16julQ9.c
+++ Cprograms/class16julQ9.c
@@ -14,36 +14,49 @@ int main()
   struct student s1;
   struct student s2;
   struct student s3;
+  
   printf("Enter the information about s1:\n");
+
   fgets(s1.name,25,stdin);
   fgets(s1.course,25,stdin);
   scanf("%lf",&s1.mobile);
   scanf("%d",&s1.reg);
+
   printf("Enter the information about s2: \n");
+
   fgets(s2.name,25,stdin);
   fgets(s2.course,25,stdin);
   scanf("%lf",&s2.mobile);
   scanf("%d",&s2.reg);
+
   printf("Enter the information about s3: \n");
+
   fgets(s3.name,25,stdin);
   fgets(s3.course,25,stdin);
   scanf("%lf",&s3.mobile);
   scanf("%d",&s3.reg);
+
   printf("Information about S1 is :\n");
+
   printf("Name - %s\n",s1.name);
   printf("course name - %s\n",s1.course);
   printf("Mobile numuber- %lf\n",s1.mobile);
   printf("Reg No- %d\n",s1.reg);
+
   printf("Information about S2 is :\n");
+
   printf("Name - %s\n",s2.name);
   printf("course name - %s\n",s2.course);
   printf("Mobile numuber- %lf\n",s2.mobile);
   printf("Reg No- %d\n",s2.reg);
+
   printf("Information about S3 is :\n");
+
   printf("Name - %s\n",s3.name);
   printf("course name - %s\n",s3.course);
   printf("Mobile numuber- %lf\n",s3.mobile);
   printf("Reg No- %d\n",s3.reg);
+
   return 0;
 }
 */
